44bd63f4a60edf0469c17bd5ba4df3e32f0dca3e
Alexander Cepeda <arlequin71@Alexanders-MacBook-Pro-2.local>
2025-12-23 15:45:08 -0300

    Clean repo: remove build/restore artifacts, add safe config template, update HTML + gitignore

--- src/config.template.js
+++ src/config.template.js
@@ -20,3 +20,4 @@ window.SENSE_DASH_CONFIG = {
     }
   }
 };
+
--- src_backup_20251220_131500/auth/authConfig.ts
+++
@@ -1,8 +0,0 @@
-export const cognitoConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_9KBwduKL5",
-  client_id: "11l9t9Bp/rahJpz57hotqv8skn",
-  redirect_uri: "https://dy18xbixpgd5w8a.cloudfront.net",
-  post_logout_redirect_uri: "https://dy18xbixpgd5w8a.cloudfront.net",
-  response_type: "code",
-  scope: "openid email phone",
-};
